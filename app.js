d3f831c3f0e90a836f2493fbb7bbbfc4a428a957
Muhammad Awais <awais@hrbeu.edu.cn>
2022-06-19 20:18:59 +0500

    Mobile Landing page project with css and vanilla javascript

---
+++ app.js
@@ -0,0 +1,22 @@
+const circle = document.getElementById("circle");
+const upBtn = document.getElementById("upBtn");
+const downBtn = document.getElementById("downBtn");
+
+
+let rotateValue = circle.style.transform;
+let rotateSum;
+
+upBtn.addEventListener("click", () => {
+
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+});
+
+downBtn.addEventListener("click", () => {
+
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+});
+
